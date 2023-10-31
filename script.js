36167f42cd9afe00ac710410819ff39aa331f4c2
Vitória Levoratto <105326710+VitoriaLevoratto@users.noreply.github.com>
2023-10-31 02:38:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function calcular() {
     const F = Number(document.querySelector("#inputfrequencia").value); // frequência
-    const C = Number(document.querySelector("#inputcomprimento").value); // comprimento
+    const C = Number(document.querySelector("#inputcomprimento").value); // comprimento da onda
     
-    const v = F * C //fórmula
+    const v = F * C //fórmula para que seja calculado a velocidade da propagação da onda
 
-    document.querySelector("#resultado").textContent = "V = velocidade: " + v.toFixed(2) + "m/s"; // oque faz o resultado aparecer
+    document.querySelector("#resultado").textContent = "V = velocidade: " + v.toFixed(2) +  "m/s"; // organiza a maneira que o resultado será apresentado e faz com que o resultado seja exibido na tela
    
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     const calcularButton = document.querySelector("#calcular");
-    calcularButton.addEventListener("click", calcular); // oque faz o botão calcular a fórmula
+    calcularButton.addEventListener("click", calcular); // faz com que, quando o botão seja clicado, calcule a fórmula
 });
